c79539cd5637fe3d94a0de3e7eac4b457b638e3d
RaixsG <jhardelloficial@gmail.com>
2024-01-09 12:39:08 -0500

    Refactor list_tasks.html and add create_tasks, delete_task, and edit_task views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import list_tasks, create_tasks
+from .views import list_tasks, create_tasks, delete_task, edit_task
 
 urlpatterns = [
     path('', list_tasks),
-    path('new/', create_tasks)
+    path('new/', create_tasks, name='create_tasks'),
+    path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
+    path('edit_task/<int:task_id>', edit_task, name='edit_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,10 +4,22 @@ from .models import Task
 
 # Create your views here.
 def list_tasks(request):
-    return render(request, 'list_tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'list_tasks.html', {'tasks': tasks})
 
 def create_tasks(request):
     # print(request.POST)
     task = Task(title=request.POST['title'], description=request.POST['description'])
+    task.save()
+    return redirect('/tasks/')
+
+def delete_task(request, task_id):
+    # print(task_id)
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('/tasks/')
+
+def edit_task(request, task_id):
+    task = Task.objects.filter(id=task_id).update(title=request.POST['title'], description=request.POST['description'])
     task.save()
     return redirect('/tasks/')

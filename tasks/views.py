c86da54c35454bee94d6eff39f8652537009d20f
RaixsG <jhardelloficial@gmail.com>
2024-01-08 19:07:59 -0500

    Add task creation functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
+# Models
+from .models import Task
 
 # Create your views here.
 def list_tasks(request):
     return render(request, 'list_tasks.html')
 
 def create_tasks(request):
-    print(request.POST)
+    # print(request.POST)
+    task = Task(title=request.POST['title'], description=request.POST['description'])
+    task.save()
     return redirect('/tasks/')

9d0dd8f29894e2ba1329d1e0275aefa669a3f3b1
RaixsG <jhardelloficial@gmail.com>
2024-01-09 19:32:11 -0500

    Update list_tasks.html and views.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import list_tasks, create_tasks, delete_task, edit_task
+from .views import list_tasks, create_tasks, delete_task
 
 urlpatterns = [
     path('', list_tasks),
     path('new/', create_tasks, name='create_tasks'),
     path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
-    path('edit_task/<int:task_id>', edit_task, name='edit_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,22 +4,27 @@ from .models import Task
 
 # Create your views here.
 def list_tasks(request):
-    tasks = Task.objects.all()
-    return render(request, 'list_tasks.html', {'tasks': tasks})
+    # print(request.POST)
+    taskToEdit = None
+    tasks = Task.objects.all().order_by('-id')
+    if request.method == 'POST':
+        taskToEdit = Task.objects.get(id=request.POST['id'])
+    return render(request, 'list_tasks.html', {'tasks': tasks, 'taskToEdit': taskToEdit})
 
 def create_tasks(request):
-    # print(request.POST)
-    task = Task(title=request.POST['title'], description=request.POST['description'])
-    task.save()
+    print(request.POST)
+    typeMethod = None
+    print(request.POST['modo'])
+    if request.POST['modo'] == 'guardar':
+        task = Task(title=request.POST['title'], description=request.POST['description'])
+        task.save()
+    else:
+        if Task.objects.filter(id=request.POST['id']).exists():
+            Task.objects.filter(id=request.POST['id']).update(title=request.POST['title'], description=request.POST['description'])
     return redirect('/tasks/')
 
 def delete_task(request, task_id):
     # print(task_id)
     task = Task.objects.get(id=task_id)
     task.delete()
-    return redirect('/tasks/')
-
-def edit_task(request, task_id):
-    task = Task.objects.filter(id=task_id).update(title=request.POST['title'], description=request.POST['description'])
-    task.save()
     return redirect('/tasks/')
